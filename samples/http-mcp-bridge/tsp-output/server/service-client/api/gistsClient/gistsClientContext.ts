1fee90d87e812788e2ef66e8818a2ac3053f7a67
Timothee Guerin <tiguerin@microsoft.com>
2025-06-05 10:48:05 -0700

    Connect github auth

--- samples/http-mcp-bridge/tsp-output/server/service-client/api/gistsClient/gistsClientContext.ts
+++ samples/http-mcp-bridge/tsp-output/server/service-client/api/gistsClient/gistsClientContext.ts
@@ -1,18 +1,24 @@
-import { type Client, type ClientOptions, getClient } from "@typespec/ts-http-runtime";
+import { type ApiKeyCredential, type Client, type ClientOptions, getClient } from "@typespec/ts-http-runtime";
 
 export interface GistsClientContext extends Client {
 
 }export interface GistsClientOptions extends ClientOptions {
   endpoint?: string;
 }export function createGistsClientContext(
+  credential: ApiKeyCredential,
   options?: GistsClientOptions,
 ): GistsClientContext {
   const params: Record<string, any> = {
-    endpoint: options?.endpoint ?? "https://api.github.com"
+
   };
-  const resolvedEndpoint = "{endpoint}".replace(/{([^}]+)}/g, (_, key) =>
+  const resolvedEndpoint = "https://api.github.com".replace(/{([^}]+)}/g, (_, key) =>
     key in params ? String(params[key]) : (() => { throw new Error(`Missing parameter: ${key}`); })()
   );;return getClient(resolvedEndpoint,{
-    ...options,
+    ...options,credential,authSchemes: [{
+      kind: "apiKey",
+      apiKeyLocation: "header",
+      name: "Authorization"
+    },
+    ]
   })
 }
