56bf3e40e5d6b693ba9cde53a396effbb93d356b
Timothee Guerin <tiguerin@microsoft.com>
2025-06-13 10:10:41 -0700

    regen

--- packages/mcp-server-typespec/tsp-output/typespec-mcp-server-js/ts-types.ts
+++ packages/mcp-server-typespec/tsp-output/typespec-mcp-server-js/ts-types.ts
@@ -3,12 +3,27 @@ export type Workflow = "mcp" | "rest api" | "rest api with js server" | "rest ap
 export type KnownEmitters = "@typespec/openapi3" | "@typespec/http-client-csharp" | "@typespec/http-client-js" | "@typespec/http-client-python" | "@typespec/http-client-java" | "@typespec/http-client-go" | "typespec-mcp";
 
 export interface InitOptions {
-  outDir: string
-  name?: string
-  workflow?: Workflow
+  /**
+   * Absolute path to the output directory where the project should be created.
+   */
+  outDir: string;
+  /**
+   * Name of the project. Default to the outDir name if not specified.
+   */
+  name?: string;
+  /**
+   * Workflow needed.
+   */
+  workflow?: Workflow;
+  /**
+   * Additional emitters to enable
+   */
   additionalEmitters?: Array<KnownEmitters | string>;
 }
 
 export interface CompileOptions {
+  /**
+   * Entrypoint to build
+   */
   entrypoint: string;
 }
