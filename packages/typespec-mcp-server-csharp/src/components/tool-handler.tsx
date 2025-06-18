b5d9cbd1a1353a38f7aeaa0afb99aa5e0d3e2a2c
Timothee Guerin <tiguerin@microsoft.com>
2025-06-18 09:07:44 -0700

    Add support for tool annotations (#55)

--- packages/typespec-mcp-server-csharp/src/components/tool-handler.tsx
+++ packages/typespec-mcp-server-csharp/src/components/tool-handler.tsx
@@ -66,9 +66,22 @@ function ToolAttributes(props: ToolMethodProps) {
   const { $ } = useTsp();
   return (
     <>
-      [McpServerTool(Name = "{props.tool.id}"), <DescriptionAttribute doc={getDoc($.program, props.tool.originalOp)} />]
+      [<McpServerToolAttribute tool={props.tool} />
+      {", "}
+      <DescriptionAttribute doc={getDoc($.program, props.tool.originalOp)} />]
     </>
   );
+}
+
+function McpServerToolAttribute(props: ToolMethodProps) {
+  const values = [
+    `Name = "${props.tool.id}"`,
+    !props.tool.annotations?.destructiveHint && "Destructive = false",
+    !props.tool.annotations?.openWorldHint && "OpenWorld = false",
+    props.tool.annotations?.idempotentHint && "Idempotent = true",
+    props.tool.annotations?.readonlyHint && "Readonly = true",
+  ];
+  return `McpServerTool(${values.filter((x) => x).join(", ")})`;
 }
 
 export interface DescriptionAttributeProps {
