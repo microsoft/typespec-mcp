5f0d8f32491d8034be8599a870b4a42e522d0e76
Timothee Guerin <tiguerin@microsoft.com>
2025-06-18 14:10:17 -0700

    Make all tool async with cancellation token (#58)

--- samples/vector-cs/tools/Math.impl.cs
+++ samples/vector-cs/tools/Math.impl.cs
@@ -3,15 +3,16 @@ using ModelContextProtocol.Server;
 namespace Mcp;
 
 
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
 
 public class MathImpl : IMath
 {
-    public Vec3 addVector(Vec3 v1, Vec3 v2)
+    public async Task<Vec3> addVectorAsync(Vec3 v1, Vec3 v2, CancellationToken cancellationToken)
     {
         return new Vec3 { x = v1.x + v2.x, y = v1.y + v2.y, z = v1.z + v2.z };
     }
 
-    public Vec3 crossProduct(Vec3 v1, Vec3 v2)
+    public async Task<Vec3> crossProductAsync(Vec3 v1, Vec3 v2, CancellationToken cancellationToken)
     {
         return new Vec3
         {
@@ -21,12 +22,12 @@ public class MathImpl : IMath
         };
     }
 
-    public double dotProduct(Vec3 v1, Vec3 v2)
+    public async Task<double> dotProductAsync(Vec3 v1, Vec3 v2, CancellationToken cancellationToken)
     {
         return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
     }
 
-    public Vec3 subVector(Vec3 v1, Vec3 v2)
+    public async Task<Vec3> subVectorAsync(Vec3 v1, Vec3 v2, CancellationToken cancellationToken)
     {
         return new Vec3 { x = v1.x - v2.x, y = v1.y - v2.y, z = v1.z - v2.z };
     }
