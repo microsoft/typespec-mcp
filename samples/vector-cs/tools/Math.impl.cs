7ba5d41da5a15c42966b8cacd30eb678225534bd
Timothee Guerin <tiguerin@microsoft.com>
2025-06-18 14:08:49 -0700

    impl

--- samples/vector-cs/tools/Math.impl.cs
+++ samples/vector-cs/tools/Math.impl.cs
@@ -3,10 +3,10 @@ using ModelContextProtocol.Server;
 namespace Mcp;
 
 
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
 
 public class MathImpl : IMath
 {
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
     public async Task<Vec3> addVectorAsync(Vec3 v1, Vec3 v2, CancellationToken cancellationToken)
     {
         return new Vec3 { x = v1.x + v2.x, y = v1.y + v2.y, z = v1.z + v2.z };
@@ -32,4 +32,3 @@ public class MathImpl : IMath
         return new Vec3 { x = v1.x - v2.x, y = v1.y - v2.y, z = v1.z - v2.z };
     }
 }
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
