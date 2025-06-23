0df0ebf9f1cdc8dea59edfe0b1a00bd0df201baf
Timothee Guerin <tiguerin@microsoft.com>
2025-06-23 09:53:59 -0700

    clean

--- samples/http-mcp-bridge/tsp-output/cs-server/generated/models/CreateGist.cs
+++
@@ -1,9 +0,0 @@
-namespace Mcp
-{
-    public class CreateGist
-    {
-        public string Description { get; set; }
-        public bool Public { get; set; }
-        public IDictionary<string, <Unresolved Symbol>> Files { get; set; }
-    }
-}
