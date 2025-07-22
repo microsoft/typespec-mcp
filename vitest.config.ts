79bc8ea500f8a4430910bd7204127cb67483c70a
Timothee Guerin <tiguerin@microsoft.com>
2025-07-22 08:52:01 -0700

    MIgrate vitest projects

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,26 @@
 import { defineConfig, mergeConfig } from "vitest/config";
-import { defaultTypeSpecVitestConfig } from "./vitest.workspace.js";
 
-export default mergeConfig(defaultTypeSpecVitestConfig, defineConfig({}));
+/**
+ * Default Config For all TypeSpec projects using vitest.
+ */
+export const defaultTypeSpecVitestConfig = defineConfig({
+  test: {
+    environment: "node",
+    isolate: false,
+    exclude: ["**/node_modules", "dist/**/*.test.*", "temp/**/*.test.*"],
+  },
+  server: {
+    watch: {
+      ignored: [],
+    },
+  },
+});
+
+export default mergeConfig(
+  defaultTypeSpecVitestConfig,
+  defineConfig({
+    test: {
+      projects: ["packages/*/vitest.config.ts"],
+    },
+  }),
+);
